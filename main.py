d5a21976e3ec04026467f9ec24feec75f1426e46
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2022-03-25 22:14:30 +0700

    commit coba

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from textblob import TextBlob
 import numpy as np
-# aku = TextBlob("I havv goood speling! My namee is Koray Tuğberk Gübür")
-# tester=aku.correct();
-# print(tester)
-w = TextBlob('Platform')
-print(w.correct())
+import os
+from spellchecker import spellchecker
+# a = input("Masukan kalimat= ")
+# textBlb = TextBlob (a)            # Making our first textblob
+# kalimat=textBlb.correct()
+# print("kalimat awal= ", a)
+# print("Kalimat benar= ", kalimat)
+# I havv goood speling! My namee is Gerry Guinardi
+with open("C://xampp/htdocs/Project_NLP/Project_NLP/tester.txt") as f:        # Opening the test file with the intention to read
+    text = f.read()                     # Reading the file
+    textBlb = TextBlob(text)            # Making our first textblob
+    textCorrected = textBlb.correct()   # Correcting the text
+    print(textCorrected)
 
-# blob = TextBlob("Analytics Vidhya is a great platform to learn data science. \n It helps community through blogs, hackathons, discussions,etc.")
-# print(blob)
+
+def compare(text1, text2):  
+    l1 = text1.split()
+    l2 = text2.split()
+    good = 0
+    bad = 0
+    for i in range(0, len(l1)):
+        if l1[i] != l2[i]:
+            bad += 1
+        else:
+            good += 1
+    return (good, bad)
+
+# Helper function to calculate the percentage of misspelled words
+def percentageOfBad(x):
+    return (x[1] / (x[0] + x[1])) * 100
